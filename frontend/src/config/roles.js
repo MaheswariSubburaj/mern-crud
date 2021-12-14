e6a3f61db4e9fddd55435b716f5dc19acc721ce7
MaheswariSubburaj <maheswari.ksubburaj@gmail.com>
2021-12-14 14:41:03 -0700

    Roles path created.

---
+++ frontend/src/config/roles.js
@@ -0,0 +1,24 @@
+import React from 'react';
+
+const Roles = () => (
+    <div>
+        admin={
+            routes = [
+                {
+                    element= 'CreateStudent',
+                    path= '/create-student'
+                },
+            ],
+        },
+        user={
+            routes = [
+                {
+                    element= 'StudentList',
+                    path= '/student-list'
+                },
+            ],
+    },
+    </div>
+);
+
+export default Roles;
