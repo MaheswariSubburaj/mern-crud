0c66a78ce1fdce7951473235aa421acbc4502824
MaheswariSubburaj <maheswari.ksubburaj@gmail.com>
2021-12-15 13:56:39 -0700

    Mongo db file.

--- backend/database/db.js
+++ backend/database/db.js
@@ -1,3 +1,7 @@
-const db = 'mongodb://localhost:27017/reactdb';
+const db = 'mongodb://mern:mern@mongo1:27017/reactdb?authSource=reactdb&replicaSet=rs0';
 
 export default db;
+
+// db.createUser({user:"mern",pwd:"mern",roles:[{role:"readWrite",db:"reactdb"}]});
+// mongosh -u localRoot -p localRootPassword --authenticationDatabase admin
+// docker exec -it mongo1 mongosh -u localRoot -p localRootPassword --authenticationDatabase admin --eval 'db.createUser({user:"test",pwd:"mern",roles:[{role:"readWrite",db:"test"}]});'
